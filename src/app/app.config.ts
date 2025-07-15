059ebc4aa134641016223d9620a07c4294337f3d
Frederik Stapelfeld <frederikstapelfeld@gmail.com>
2025-07-15 21:47:03 +0200

    init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,15 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideKirby, withGlobalSetup } from '@kirbydesign/designsystem';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideKirby(withGlobalSetup()),
+    provideAnimations(),
     provideRouter(routes),
-    provideZoneChangeDetection()
+    provideZoneChangeDetection(),
 
   ]
 };

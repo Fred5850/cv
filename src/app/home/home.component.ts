33f349fd7b4ffdac9e6afecad2fd6cea8b4eba1c
Frederik Stapelfeld <frederikstapelfeld@gmail.com>
2025-07-16 19:56:45 +0200

    Update home component layout to include contact information and skills; enhance imports for Avatar and Divider components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import {  CardModule, CheckboxComponent, HeaderModule, IconModule, ItemModule, KirbyModule, PageModule } from '@kirbydesign/designsystem';
+import { CardModule, CheckboxComponent, HeaderModule, IconModule, ItemModule, KirbyModule, PageModule, DividerComponent, AvatarComponent } from '@kirbydesign/designsystem';
 
 @Component({
-  imports: [     
+  imports: [
     PageModule,
     ItemModule,
     IconModule,
     CheckboxComponent,
     HeaderModule,
-    CardModule
-  ],
+    CardModule,
+    DividerComponent,
+    AvatarComponent
+],
   templateUrl: './home.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
